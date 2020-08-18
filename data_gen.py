7cffecef7aed35788a962be9c876f424dbc72a32
Harsh Parikh <harsh081193@gmail.com>
2020-08-18 17:04:16 +0530

    estimation

--- data_gen.py
+++ data_gen.py
@@ -11,19 +11,19 @@ import pandas as pd
 from itertools import product 
 import scipy.special
 
-def generate_citizen_table(n=500):
+def generate_citizen_table(n=2000):
     citizen = pd.DataFrame()
     citizen['age'] = np.random.poisson( lam=30, size=n )
     citizen['gender'] = np.random.binomial( 1, 0.5, size=n )
     citizen['income'] = np.random.uniform(low=10,high=100, size=n)
     return citizen
 
-def generate_lender_table(n=5):
+def generate_lender_table(n=20):
     lender = pd.DataFrame()
     lender['asset'] = np.random.poisson( lam=1500, size=n )
     return lender
 
-def generate_locality_table(n=10):
+def generate_locality_table(n=15):
     locality = pd.DataFrame()
     locality['lat'] = np.random.uniform(21,25,n)
     locality['long'] = np.random.uniform(75,80,n)
@@ -39,7 +39,7 @@ def generate_residency(citizens,localities):
         residency.loc[citizen_idx[i],residency_map[i]] = 1.0
     return residency, residency_map
 
-def generate_borrow_groups(citizens,residency_map,n=20):
+def generate_borrow_groups(citizens,residency_map,n=125):
     df = citizens
     df['residency'] = residency_map
     borrow = pd.DataFrame()
@@ -69,13 +69,45 @@ def generate_social_network(citizens,residency_map):
                               - a2*np.abs(citizens['income'].loc[i]-citizens['income'].loc[j]) )
                 a_ij = np.random.binomial(1, p_ij)
                 if a_ij>0:
-                    net = net.append([[i,j,p_ij],[j,i,p_ij]])
+                    net = net.append([[i,j],[j,i]],ignore_index=True)
     return net
+
+def generate_loans(b_idx,lenders):
+    lender_idx = lenders.index
+    loan_map = np.random.choice(lender_idx,size=len(b_idx),replace=True)
+    loans = pd.DataFrame(loan_map,index=b_idx)
+    return loans, loan_map
+
+def get_clique(array):
+    clique = []
+    for i in array:
+        for j in array:
+            if i!=j:
+                clique.append([i,j])
+                # clique.append([j,i])
+    return pd.DataFrame(clique)
+
+def add(edges,new_edges):
+    edges_1 = edges.copy(deep=True)
+    for i in new_edges.index:
+        if not (edges_1 == new_edges.loc[i]).all(1).any():
+            edges_1 = edges_1.append(new_edges.loc[i],ignore_index=True)
+    return edges_1
+
+def intervention(b_idx,borrowing_groups_rel,social_net):
+    for i in b_idx:
+        bg = borrowing_groups_rel.loc[borrowing_groups_rel[0]==i,1].to_numpy()
+        clique = get_clique(bg)
+        social_net = add(social_net[[0,1]],clique)
+    return social_net
+
+def generate_repayment(borrowing_groups,borrowing_groups_rel,loans,social_net):
+    a0, a1 = (0.1,250)
+    rr = []
+    for i in borrowing_groups.index:
+        bg = borrowing_groups_rel.loc[borrowing_groups_rel[0]==i,1]
+        potential_edges = get_clique(bg.to_numpy())
+        edges = sum([ ((social_net[[0,1]] == potential_edges.loc[i]).all(1).any()) for i in potential_edges.index ])
+        rr.append(max(0,a0*100*(edges/len(potential_edges)) + a1/(loans.loc[i,'interest']+1) ))
+    return rr
     
-np.random.seed(0)
-citizens = generate_citizen_table()
-lender = generate_lender_table()
-localities = generate_locality_table()
-residency, residency_map = generate_residency(citizens, localities)
-borrower_idx, borrowing_groups = generate_borrow_groups(citizens,residency_map)
-social_net = generate_social_network(citizens,residency_map)
---
+++ eval.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 18 01:37:25 2020
+
+@author: harspari
+"""
+
+from data_gen import *
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+
+#STRUCTURAL EQN
+ 
+'''
+social_net(C1,C2) <- age[C1], age[C2], income[C1], income[C2], residence[C1], residence[C2]
+borrowing_group_rel(BG,C) <- residence[C]
+
+capital[BG] <- income[C], age[C] where borrowing_group_rel(BG,C)
+loan_amount[BG] <- size[BG], capital[BG]
+loan_interest[BG] <- asset[L] where loan(BG,L)
+
+repayment[BG] <- loan_interest[BG], social_net(C1,C2) where borrowing_group_rel(BG,C1) and borrowing_group_rel(BG,C2)
+'''
+
+def learn_capital(borrowing_groups, borrowing_groups_rel, citizens):
+    b_idx = borrowing_groups.index
+    Y = borrowing_groups['capital']
+    X = pd.DataFrame()
+    for i in b_idx:
+        c_idx = borrowing_groups_rel.loc[borrowing_groups_rel['borrowing_group']==i,'citizen']
+        df = citizens.loc[c_idx][['age','income']]
+        X = X.append(df.mean(axis=0),ignore_index=True)
+    rf = RandomForestRegressor().fit(X, Y)
+    return lambda x: rf.predict(x)
+
+def learn_amount(borrowing_groups, borrowing_groups_rel, loans):
+    Y = loans['amount']
+    X = pd.DataFrame()
+    b_idx = borrowing_groups.index
+    size = []
+    for i in b_idx:
+        size.append(len(borrowing_groups_rel.loc[borrowing_groups_rel['borrowing_group']==i,'citizen']))
+    borrowing_groups['size'] = size
+    X['size'] = size
+    X['capital'] = borrowing_groups['capital']
+    rf = RandomForestRegressor().fit(X, Y)
+    return lambda x: rf.predict(x)
+
+def learn_interest(loans, lenders):
+    Y = loans['interest']
+    X = lenders.loc[loans['lender']]
+    rf = RandomForestRegressor().fit(X, Y)
+    return lambda x: rf.predict(x)
+
+def learn_repayment(borrowing_groups, loans, borrowing_groups_rel, social_net):
+    Y = borrowing_groups['repayment']
+    X = pd.DataFrame()
+    X['interest'] = loans['interest']
+    b_idx = borrowing_groups.index
+    K = []
+    for i in b_idx:
+        c_idx = borrowing_groups_rel.loc[borrowing_groups_rel['borrowing_group']==i,'citizen']
+        tuples = get_clique(c_idx.to_numpy())
+        k = np.mean([ ((social_net.to_numpy() == tuples.loc[i].to_numpy()).all(1).any()) for i in tuples.index ])
+        K.append(k)
+    X['social_net'] = K
+    rf = LinearRegression().fit(X, Y)
+    return lambda x: rf.predict(x)
+
+def estimate_repayment( f, borrowing_groups, loans, borrowing_groups_rel, social_net ):
+    X = pd.DataFrame()
+    X['interest'] = loans['interest']
+    b_idx = borrowing_groups.index
+    K = []
+    for i in b_idx:
+        c_idx = borrowing_groups_rel.loc[borrowing_groups_rel['borrowing_group']==i,'citizen']
+        tuples = get_clique(c_idx.to_numpy())
+        k = np.mean([ ((social_net.to_numpy() == tuples.loc[i].to_numpy()).all(1).any()) for i in tuples.index ])
+        K.append(k)
+    X['social_net'] = K
+    Y = f(X)
+    return X, Y
+
+
+## DATA GENERATION
+np.random.seed(0)
+citizens = generate_citizen_table()
+lenders = generate_lender_table()
+localities = generate_locality_table()
+
+residency, residency_map = generate_residency(citizens, localities)
+borrower_idx, borrowing_groups_rel = generate_borrow_groups(citizens,residency_map)
+social_net = generate_social_network(citizens,residency_map)
+loans, loan_map = generate_loans(borrower_idx,lenders)
+borrowing_groups = pd.DataFrame( index = borrower_idx )
+
+#SETTING UP RELATIONSHIPS
+borrowing_groups['capital'] = [np.mean([ citizens.loc[i,'income']*10/citizens.loc[i,'age'] 
+                               for i in borrowing_groups_rel.loc[borrowing_groups_rel[0] == j,1].to_numpy() ]) 
+                               for j in borrower_idx]
+loans['amount'] = [len(borrowing_groups_rel.loc[borrowing_groups_rel[0] == j,1])*borrowing_groups['capital'].loc[j]/10 
+                   for j in loans.index]
+loans['interest'] = [ np.exp(-lenders.loc[loans[0].loc[j]]['asset']/500)*100 for j in loans.index ]
+
+
+borrowing_groups['repayment'] = generate_repayment(borrowing_groups,borrowing_groups_rel,loans,social_net)
+
+# INTERVENTION
+social_net_1 = intervention( borrower_idx, borrowing_groups_rel, social_net)
+
+#TRUE POTENTIAL OUTCOME (do not use it for anything except comparing the output)
+borrowing_groups_1 = borrowing_groups.copy(deep=True)
+borrowing_groups_1['repayment'] = generate_repayment(borrowing_groups,borrowing_groups_rel,loans,social_net_1)
+
+#RENAME COLUMNS
+loans.rename(columns={0:'lender'},inplace=True)
+borrowing_groups_rel.rename(columns={0:'borrowing_group',1:'citizen'},inplace=True)
+social_net.rename(columns={0:'citizen_0',1:'citizen_1'},inplace=True)
+social_net_1.rename(columns={0:'citizen_0',1:'citizen_1'},inplace=True)
+
+
+## LEARNING CONDITIONAL DISTRIBUTIONS (STRUCTURAL EQNS)
+f_capital = learn_capital(borrowing_groups, borrowing_groups_rel, citizens) #f_capital: s(age x income) -> capital
+f_amount = learn_amount(borrowing_groups, borrowing_groups_rel, loans) #f_amount: size x capital -> amount
+f_interest = learn_interest(loans, lenders) #f_interest: asset -> interest
+
+f_repayment = learn_repayment(borrowing_groups, loans, borrowing_groups_rel, social_net) #f_repayment: interest x s(social_net) -> repayment
+
+#ESTIMATING POTENTIAL OUTCOMING
+X0, repayment0 = estimate_repayment( f_repayment, borrowing_groups, loans, borrowing_groups_rel, social_net )
+X1, repayment1 = estimate_repayment( f_repayment, borrowing_groups, loans, borrowing_groups_rel, social_net_1 )
+
+fig = plt.figure()
+plt.scatter(X0['social_net'],repayment0)
+plt.scatter(X0['social_net'],borrowing_groups['repayment'])
+plt.legend(['estimated','truth'])
+
+fig = plt.figure()
+plt.plot([0,100],[0,100],c='black')
+plt.scatter(borrowing_groups['repayment'],repayment0)
+plt.scatter(borrowing_groups_1['repayment'],repayment1)
+plt.legend(['x=y','control','treated'])
